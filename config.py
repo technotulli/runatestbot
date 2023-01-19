a3e7a20d998c1211aa18c0e05e52aeb0e0c9dcc7
Gaurav-4x <117690973+Gaurav-4x@users.noreply.github.com>
2023-01-19 21:21:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001854836916"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "2079962173"))
 
 #Port
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "70")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://manju:1234@cluster0.s6qpf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
